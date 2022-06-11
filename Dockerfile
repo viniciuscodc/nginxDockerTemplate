15e77013a93eda81967cdea383dbfc90f535bc9f
viniciuscodc <viniciuscodc@gmail.com>
2022-06-11 17:51:51 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# syntax=docker/dockerfile:1
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+WORKDIR /app
+
+# Copy csproj and restore as distinct layers
+COPY ./NgnixTest/NgnixTest.csproj ./
+RUN dotnet restore
+
+# Copy everything else and build
+COPY ./ ./
+RUN dotnet publish NgnixTest.sln -c Release -o out
+
+# Build runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+WORKDIR /app
+
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "NgnixTest.dll"]
---
+++ NgnixTest/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
